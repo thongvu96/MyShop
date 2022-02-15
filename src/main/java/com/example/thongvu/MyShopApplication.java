af236ac568d7fda13011f3c342f060cf36472a20
vuthong <vuthong@luvina.net>
2022-02-15 14:42:15 +0700

    create project

---
+++ src/main/java/com/example/thongvu/MyShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.thongvu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thongvu/MyShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thongvu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
